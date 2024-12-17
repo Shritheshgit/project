c38991703751d9ddc1071cb84f8427589f82170d
Shritheshgit <shritheshsuvarna17@gmail.com>
2024-12-17 10:33:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@mui/material';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <Box>
+      <Home/>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { Box, Grid2, Paper, Typography } from '@mui/material'
+import InsertForm from './InsertForm'
+import List from './List'
+import { MoveToInbox } from '@mui/icons-material';
+
+export default function Home() {
+    let initialValue;
+    if (localStorage.getItem("notes")==null) {
+        initialValue=[]
+    } else {
+        initialValue=JSON.parse(localStorage.getItem("notes"))
+    }
+    const[notes,setNotes]=useState(initialValue)
+    const deleteNote=(id)=>{
+        console.log(id)
+        let remainingNotes=notes.filter((item)=>item.id!=id)
+        localStorage.setItem("notes",JSON.stringify(remainingNotes))
+        setNotes(remainingNotes)
+    }
+    const updateNote=(note)=>{
+        console.log(note)
+        let status;
+        if (note.completed) {
+            status=false;
+        } else {
+           status=true; 
+        }
+        let modifiedNote={...note,completed:status}
+        console.log(modifiedNote)
+        let noteIndex=notes.findIndex((item)=>item.id==note.id)
+        console.log(noteIndex)
+        let modifiedCompleteNote=[...notes]
+        modifiedCompleteNote.splice(noteIndex,1,modifiedNote)
+        localStorage.setItem("notes",JSON.stringify(modifiedCompleteNote))
+        setNotes(modifiedCompleteNote)
+    }
+    return (
+        <Box
+            sx={{
+                backgroundColor: "purple",
+                width: "100%",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Paper sx={{p:5,  fontFamily:"cursive",borderRadius:"20px"}}>
+                <Box>
+                    <Grid2 container spacing={2}></Grid2>
+                    <Grid2 size={{xs:12}}>
+                        <Typography 
+                        sx={{
+                            textAlign:"center",
+                            fontWeight:"600",
+                            color:"purple",
+                            textTransform:"uppercase",
+                            }}
+                            >
+                               TO DO LIST
+                                </Typography>
+                    </Grid2>
+                    <Grid2 size={{xs:12}}>
+                        <InsertForm notes={notes} setNotes={setNotes}/>
+                    </Grid2>
+                    <Grid2 size={{xs:12}}>
+                        <Box sx={{maxHeight:"40vh",overflow:"auto"}}>
+                            {notes.length>0?
+                            (notes.map((note,index)=>(
+                            <List 
+                            updateNote={updateNote}
+                            deleteNote={deleteNote}
+                            key={index} 
+                            note={note}/>   
+                            ))):(
+                              <Typography color='text.secondary' 
+                        sx={{textAlign:'center'}}>
+                            NO NOTES FOUND
+                        </Typography>  
+                            )}
+                        </Box>
+                        
+                    </Grid2>
+                </Box>
+            </Paper>
+        </Box>
+    )
+}
